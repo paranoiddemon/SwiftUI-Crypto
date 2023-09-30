dada2eb3652fc960acfbec08833061e162d0ecf8
forys <demonx0912@gmail.com>
2023-09-30 11:53:00 +0800

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by forys on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
